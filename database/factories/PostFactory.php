49945e29294e43c9cd36016b71ecbe7fab5e044a
Ryan Lucas <ryanlucas@moo.local>
2022-10-13 09:54:06 +0100

    created post controller

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 use App\Models\User;
 use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -22,8 +23,8 @@ class PostFactory extends Factory
             'category_id' => Category::factory(),
             'title' => $this->faker->sentence,
             'slug' => $this->faker->slug,
-            'excerpt' => $this->faker->sentence,
-            'body' => $this->faker->paragraph
+            'excerpt' => '<p>' . implode('</p><p>', $this->faker->paragraphs(2)) . '</p>',
+            'body' => '<p>' . implode('</p><p>', $this->faker->paragraphs(6)) . '</p>',
         ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,20 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    \Illuminate\Support\Facades\DB::listen(function($query) {
-//        logger($query->sql, $query->bindings);
-    });
+    $posts = Post::latest();
+    if (request('search')) {
+        $posts
+            ->where('title', 'like', '%' . request('search') . '%' )
+            ->orWhere('body', 'like', '%' . request('search') . '%' );
+    }
 
 
-    return view('posts', ['posts' => Post::latest()->get()
+    return view('posts', [
+        'posts' => $posts->get(),
+        'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 // $slug catches the wildcard
 Route::get('posts/{post}', function (Post $post) { // Post::where('slug', $post)->firstOrFail()
@@ -37,12 +41,17 @@ Route::get('posts/{post}', function (Post $post) { // Post::where('slug', $post)
 // wildcard name and variable match
 Route::get('categories/{category:slug}', function(Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
+
+
     ]);
-});
+})->name('category');
 
 Route::get('authors/{author:username}', function(User $author) {
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts,
+        'categories' => Category::all()
     ]);
 });
